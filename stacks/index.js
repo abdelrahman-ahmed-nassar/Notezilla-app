0ac1523fd59ba74c7e2e10d7fc42792fce68d284
abdelrahman <abdelrhman.ahmed.nassar@gmail.com>
2022-08-08 22:59:18 +0200

    securing the API

--- stacks/index.js
+++ stacks/index.js
@@ -1,5 +1,7 @@
 import { StorageStack } from "./StorageStack";
 import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
+
 export default function main(app) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
@@ -8,5 +10,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
 }
