6f395146e07aab9f1357a8a12ea7559984e90849
abdelrahman <abdelrhman.ahmed.nassar@gmail.com>
2022-08-09 15:03:20 +0200

    Enable CORDS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,7 +1,16 @@
 import { Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
 
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
